38590736756952c68360a74f2220fbd8d5282091
Juan Sebastián Sutachán Correa <jsebastiansc935@gmail.com>
2026-08-24 16:49:18 -0500

    ci: removed s3 declaration on versions.tf until the backend is being worked on and pre-commit is running properly

--- versions.tf
+++ versions.tf
@@ -18,5 +18,7 @@ terraform {
   # backend config file (see config/backend.hcl.example) so nothing
   # environment-specific is hardcoded in the codebase.
   #   terraform init -backend-config=config/backend.hcl
-  backend "s3" {}
+  #Remove the comment once the backend is being worked on
+ # backend "s3" {}
+#
 }
